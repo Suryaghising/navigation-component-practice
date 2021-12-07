9f33ea45c23c40326280f75fb30c2953d308e17d
Surya Ghising <suryaghising2@gmail.com>
2021-12-07 15:45:40 +0545

    Navigation completed.

---
+++ app/src/main/java/com/example/navigationcomponentpractice/fragments/FragmentA.kt
@@ -0,0 +1,26 @@
+package com.example.navigationcomponentpractice.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.example.navigationcomponentpractice.R
+class FragmentA : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_a, container, false)
+        val navController = activity?.let { Navigation.findNavController(it, R.id.fragmentContainerView) }
+        val buttonA: Button = view.findViewById(R.id.buttonA)
+        buttonA.setOnClickListener {
+            navController?.navigate(R.id.action_fragmentA_to_fragmentB)
+        }
+        return  view
+    }
+
+}
---
+++ app/src/main/java/com/example/navigationcomponentpractice/fragments/FragmentB.kt
@@ -0,0 +1,27 @@
+package com.example.navigationcomponentpractice.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.example.navigationcomponentpractice.R
+
+class FragmentB : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_b, container, false)
+        val navController = activity?.let { Navigation.findNavController(it, R.id.fragmentContainerView) }
+        val buttonB: Button = view.findViewById(R.id.buttonB)
+        buttonB.setOnClickListener {
+            navController?.navigate(R.id.action_fragmentB_to_fragmentA)
+        }
+        return  view
+    }
+
+}
